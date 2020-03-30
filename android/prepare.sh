a9165c498e7875d7c41c99db07f3bfd6b406ee3d
mapledxf <xuefeng.ding@live.cn>
2020-03-30 18:16:05 +0800

    Update prepare.sh

--- android/prepare.sh
+++ android/prepare.sh
@@ -8,6 +8,8 @@ tar -xzvf android-sdk*-linux.tgz
 ln -s android-ndk-r20 android-sdk-linux/ndk-bundle
 # Need to use command line tool to get sdkmanager
 cd android-sdk-linux/
+https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip
+unzip commandlinetools-linux-*.zip
 export ANDROID_HOME=$PWD
 yes | ./tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --licenses
 
